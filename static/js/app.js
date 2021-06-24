f2a6da00a00f2e0af708ec593882333efced96a6
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-06-24 11:39:37 -0700

    Get selection

--- static/js/app.js
+++ static/js/app.js
@@ -5,32 +5,20 @@ function init() {
         var names = data.names;
         // Get reference to dropdown selection
         var select = d3.select("#selDataset");
-        console.log(select);
         names.forEach(name => 
-            select.append("option").text(name).property("value"));
-       
+            select.append("option").text(name).property("value"));       
     });
-    //buildPlot(data.names[0]);
+    // Use first id to build inital plots
+    //buildPlot(names[0]);
+    //buildData(names[0]);
+}
+// Call updatePlotly() when a change takes place to the DOM
+function optionChanged(newName) {
+    console.log(newName);
+    //buildPlot(newName);
+    //buildData(newName);   
 }
 //------------------------------------------------------------
-// function buildPlot() {
-//     d3.json("../samples.json").then((data) => { 
-//         console.log(data);
-//         console.log(data.samples);
-//          // Grab values from the response json object to build the plots
-//         var ids = data.samples.map(row => row.id);
-//         console.log(ids)
-        //         //datetimes = tableData.map(date => date.datetime);
-        //         // var otuIds = data.samples.map(row => row[1]);
-        //         // var sampleValues = data.samples.map(row => row[2]);
-        //         // var otuLabels = data.samples.map(row => row[3]);
-        //         console.log(ids);
-        //         // console.log(otuIds);
-        //         // console.log(sampleValues);
-        //         // console.log(otuLabels);
-        });
-        // } 
-}
+// function buildPlot() { }
 
 init();
-var selection = 
