34b8e7efa772585ceb84fcdb91b9ff55c56f4c8d
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-06-25 13:25:08 -0700

    Updated meta

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,8 @@
 //------------------------------------------------------------
-// Fills dropdown menu and build inital plots and info
-function init() {
-    // Populate dropdown menu with ids
-    d3.json("../samples.json").then(data => { 
-        var names = data.names;
-        // Get reference to dropdown
-        var select = d3.select("#selDataset");
-        names.forEach(name => 
-            select.append("option").text(name).property("value"));     
-        // Use first id to build inital plots and info
-        buildPlot(names[0]);
-        buildInfo(names[0]);  
-    });    
-}
-//------------------------------------------------------------
-// Executes when a change takes place to dropdown menu then builds plots and data
-function optionChanged(newName) { 
-    console.log(newName);
-    //buildPlot(newName);
-    //buildInfo(newName);   
-}
-//------------------------------------------------------------
-// Builds Plots
+// Builds Bar and Bubble Plots
 function buildPlot(name) {
     d3.json("../samples.json").then(data => { 
-        // Get this one sample's data
+        // Get this sample's data
         var thisSample = data.samples.filter(sample => sample.id == name);
         thisSample = thisSample[0];
         console.log(thisSample);
@@ -34,7 +12,7 @@ function buildPlot(name) {
         var values = thisSample.sample_values;
         var labels = thisSample.otu_labels;
         
-        // Build bar plot
+        // Build BAR
         var data = [{
             x: (values.slice(0, 10)).reverse(),
             y: (otuIds.slice(0, 10)).reverse(),
@@ -71,12 +49,35 @@ function buildInfo(name) {
     d3.json("../samples.json").then(data => { 
         var thisMeta = data.metadata.filter(meta => meta.id == name);
         console.log(thisMeta);
-        
+        // Get reference to html element, remove any existing elements, and append info
         var select = d3.select(".panel-body");
+        d3.selectAll("h5").remove();
         thisMeta.forEach(meta => {
             Object.entries(meta).forEach(([key, value]) => select.append("h5").text(`${key}: ${value}`));
         });
     });
+}
+//------------------------------------------------------------
+// Fills dropdown menu and build inital plots and info
+function init() {
+    // Populate dropdown menu with ids
+    d3.json("../samples.json").then(data => { 
+        var names = data.names;
+        // Get reference to dropdown
+        var select = d3.select("#selDataset");
+        names.forEach(name => 
+            select.append("option").text(name).property("value"));     
+        // Use first id to build inital plots and info
+        buildPlot(names[0]);
+        buildInfo(names[0]);  
+    });    
+}
+//------------------------------------------------------------
+// Executes when a change takes place to dropdown menu then builds plots and data
+function optionChanged(newName) { 
+    console.log(newName);
+    buildPlot(newName);
+    buildInfo(newName);   
 }
 //------------------------------------------------------------
 init();
